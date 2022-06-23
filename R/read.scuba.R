06c494ef3a88a56646d0c636fd71aa034765c488
TobieSurette <tobie.surette@gmail.com>
2022-06-23 10:05:37 -0300

    Update read.scuba.R

--- R/read.scuba.R
+++ R/read.scuba.R
@@ -16,11 +16,11 @@
 
 
 # "http://dmapps/en/scuba/reports/dive.log.xlsx", to = "dive log.xlsx")
-# "http://dmapps/en/scuba/reports/section/?year=2021" # Read in transect section table:
-# "http://dmapps/en/scuba/reports/dive/?year=2021" # Read dives table:
-# "http://dmapps/en/scuba/reports/outing/?year=2021" # Read outings table:
-# "http://dmapps/en/scuba/reports/observations/?year=2018" # Read biological data:
-# "http://dmapps/en/scuba/reports/scuba_transect/?year=2021" # Scuba transect table:
+# "http://dmapps/en/scuba/reports/section/?year=2021"        # Read in transect section table:
+# "http://dmapps/en/scuba/reports/dive/?year=2021"           # Read dives table:
+# "http://dmapps/en/scuba/reports/outing/?year=2021"         # Read outings table:
+# "http://dmapps/en/scuba/reports/observations/?year=2018"   # Read biological data:
+# "http://dmapps/en/scuba/reports/transects/?year=2021"      # Scuba transect table:
 
 #' @export read.scuba
 read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = TRUE){
@@ -33,54 +33,63 @@ read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = T
       if (table == "biological") table <- "observations"
    } 
    
-   # Build transect index table:
-   if (echo) cat("Building transect table.\n")
-   transects <- sort(unique(read.csv("http://dmapps/en/scuba/reports/outing/?year")$transect))
-   transects <- data.frame(transect = transects)
-   transects <- transects[transects$transect != "", , drop = FALSE]
-   transects$region <- unlist(lapply(strsplit(gsub(")", "", transects$transect), "[(]"), function(x) x[length(x)]))
-   transects$name <- transects$transect
-   transects$number <- as.numeric(unlist(lapply(strsplit(gsub("T-", "", transects$transect), " "), function(x) x[1])))
-   transects <- transects[, setdiff(names(transects), "transect")]
-   transects <- transects[, c("region", "number", "name")]
-   
-   # Read outings table:
-   if (!missing(year)){
-      outings <- NULL
-      for (i in 1:length(year)){
-         if (echo) cat(paste0("Reading outing table for ", year[i], ".\n"))
-         outings <- rbind(outings, read.csv(paste0(path, "outing/?year=", year[i])))
-      } 
-   }else{
-      outings <- read.csv(path, "outing/?year")
-   }
-   if (nrow(outings) > 0){
-      outings$date <- substr(outings$datetime, 1, 10)
-      outings$time <- substr(outings$datetime, 12, 19)
-      names(outings) <- gsub("^id$", "outing.id", names(outings))
-      names(outings) <- gsub("_", ".", names(outings))
-      names(outings) <- gsub("^transect$", "transect.name", names(outings))
-      outings$region <- transects$region[match(outings$transect.name, transects$name)]
-      outings$region[is.na(outings$region)] <- ""
-      
-      # Re-order variables:
-      start <- c("region", "date", "time", "outing.id", "transect.name")
-      end <- c("weather.notes", "comment")
-      outings <- outings[, c(start, setdiff(names(outings), c(start, end)), end)]
-      
-      # Remove empty rows:
-      remove <- c("datetime", "transect.id")
-      outings <- outings[, setdiff(names(outings), remove)]   
-      
-      if (compress){
-         # Remove empty columns:
-         outings <- gulf.utils::compress(outings)
-         
-         # Remove empty rows:
-         outings <- outings[which(outings$transect != ""), ]
-      } 
-   }
+   # Format outings table:
+   if (echo) cat(paste0("Reading outing table.\n"))
+   outings <- read.csv(paste0(path, "outing/?year"))
 
+   # Format variables:
+   outings$date <- substr(outings$datetime, 1, 10)
+   outings$time <- substr(outings$datetime, 12, 19)
+   names(outings) <- gsub("^id$", "outing.id", names(outings))
+   names(outings) <- gsub("_", ".", names(outings))
+   names(outings) <- gsub("^transect$", "transect.name", names(outings))
+   outings$region <- unlist(lapply(strsplit(gsub("[)]", "", outings$transect.name), "[(]"), function(x) x[2]))
+   outings$is.upm <- ifelse(outings$is.upm == "False", FALSE, ifelse(outings$is.upm == "True", TRUE, as.logical(NA)))
+   outings$is.training <- ifelse(outings$is.training == "False", FALSE, ifelse(outings$is.training == "True", TRUE, as.logical(NA)))
+   outings$longitude.start   <- outings$start.longitude
+   outings$latitude.start    <- outings$start.latitude
+   outings$longitude.end     <- outings$end.longitude
+   outings$latitude.end      <- outings$end.latitude
+   
+   # Re-order variables:
+   start <- c("region", "date", "time", "outing.id", "transect.name", "transect.id")
+   end <- c("weather.notes", "comment")
+   outings <- outings[, c(start, setdiff(names(outings), c(start, end)), end)]
+   
+   # Remove empty rows:
+   remove <- c("datetime", "start.latitude.d", "start.latitude.mm", 
+               "start.longitude.d", "start.longitude.mm", "end.latitude.d", "end.latitude.mm",
+               "end.longitude.d", "end.longitude.mm", "start.latitude", "start.longitude",
+               "end.latitude", "end.longitude") 
+   outings <- outings[, setdiff(names(outings), remove)]   
+
+   # Load and format transect table:
+   if (echo) cat(paste0("Reading transect table.\n"))
+   transects <- read.csv("http://dmapps/en/scuba/reports/transects/?year=2021")      
+   transects$region            <- gsub(" [(]PE[)]", "", transects$region)
+   transects$longitude.start   <- transects$start_longitude
+   transects$latitude.start    <- transects$start_latitude
+   transects$longitude.end     <- transects$end_longitude
+   transects$latitude.end      <- transects$end_latitude
+   transects$transect.name.old <- transects$old_name
+   transects$transect.id       <- transects$id
+   transects$description       <- transects$description_en
+   transects$transect.number   <- transects$name
+   remove <- c("description_en", "description_fr", "start_latitude_d", "start_latitude_mm",
+               "start_longitude_d", "start_longitude_mm", "end_latitude_d", "end_latitude_mm",
+               "end_longitude_d",    "end_longitude_mm", "start_longitude", "end_latitude",
+               "end_longitude", "old_name", "start_latitude", "region_id", "id", "name")
+   transects <- transects[, setdiff(names(transects), remove)]
+   names(transects) <- gsub("_", ".", names(transects))
+   ix <- match(transects$transect.id, outings$transect.id)
+   transects$transect.name <- outings$transect.name[ix]
+   start <- c("region", "transect.name", "transect.id",  "transect.name.old", "transect.number")
+   end <- c("description")
+   transects <- transects[, c(start, setdiff(names(transects), c(start, end)), end)]
+
+   # Subset by year:
+   if (!missing(year)) outings <- outings[year(outings$date) %in% year, ]
+   
    # Read dive table:
    if (!missing(year)){
       dives <- NULL
@@ -89,7 +98,7 @@ read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = T
          dives <- rbind(dives, read.csv(paste0(path, "dive/?year=", year[i])))
       } 
    }else{
-      dives <- read.csv(path, "dive/?year")
+      dives <- read.csv(paste0(path, "dive/?year"))
    }
    if (nrow(dives) > 0){
       # Format variable names:
@@ -101,9 +110,27 @@ read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = T
       dives$date <- substr(dives$start.descent, 1, 10)
       dives$start.time <- substr(dives$start.descent, 12, 19)
       dives$bottom.time.mins <- dives$bottom.time
+      
+      # Fill-in missing dates:
+      ix <- which(is.na(dives$date))
+      iy <- match(dives$outing.id[ix], outings$outing.id)
+      dives$date[ix] <- outings$date[iy]
+      
+      # Format field names:
       names(dives) <- gsub("^transect$", "transect.name", names(dives))
-      dives$region <- transects$region[match(dives$transect.name, transects$name)]
+      
+      # Format region:
+      #dives$region <- transects$region[match(dives$transect.name, transects$transect.name)]
+      dives$region <- unlist(lapply(strsplit(gsub("[)]", "", dives$transect.name), "[(]"), function(x) x[2]))
       dives$region[is.na(dives$region)] <- ""
+
+      # Add UPM and training flags:
+      ix <- match(dives$outing.id, outings$outing.id)
+      dives$is.upm <- as.logical(outings$is.upm[ix])
+      dives$is.training <- as.logical(outings$is.training[ix])
+
+      # Format logical variables:
+      dives$was.seeded <- ifelse(dives$was.seeded == "False", FALSE, ifelse(dives$was.seeded == "True", TRUE, as.logical(NA)))
       
       # Remove empty rows:
       remove <- c("created.by", "created.at", "updated.by", "updated.at", "sample", "created.by.id", 
@@ -115,8 +142,9 @@ read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = T
       end <- c("comment")
       dives <- dives[, c(start, setdiff(names(dives), c(start, end)), end)]
       
-      # Remove empty columns:
-      if (compress) dives <- gulf.utils::compress(dives)
+      # Training dive corrections:
+      ix <- grep("train", tolower(dives$comment))
+      dives$is.training[ix] <- TRUE
    }
    
    # Read section table:
@@ -127,7 +155,7 @@ read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = T
          sections <- rbind(sections, read.csv(paste0(path, "section/?year=", year[i])))
       } 
    }else{
-      sections <- read.csv(path, "section/?year")
+      sections <- read.csv(paste0(path, "section/?year"))
    }
    if (nrow(sections) > 0){
       # Format variable names:
@@ -136,15 +164,23 @@ read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = T
       names(sections) <- gsub("^id$", "section.id", names(sections))
       names(sections) <- gsub("^transect$", "transect.number", names(sections))
       
-      tmp <- sections[c("region", "transect.number")]
-      names(tmp) <- c("region", "number")
-      ix <- match(tmp, transects[c("region", "number")])
-      sections$transect.name <- transects$name[ix]
+      # Get transect name:
+      sections$transect.name <- outings$transect.name[match(sections$outing.id, outings$outing.id)]
       sections$transect.name[is.na(sections$transect.name)] <- ""
       
       # Format 'comment field:
       sections$comment[is.na(sections$comment)] <- ""
       sections$comment <- tolower(sections$comment)
+      
+      # Add UPM, training, and seeding experiment info:
+      ix <- match(sections$dive.id, dives$dive.id)
+      sections$is.upm      <- as.logical(dives$is.upm[ix])
+      sections$is.training <- as.logical(dives$is.training[ix])
+      sections$was.seeded  <- dives$was.seeded[ix]
+      
+      # Format region variable:
+      sections$region <- gsub(" [(]PE[)]", "", sections$region)
+      sections$region[is.na(sections$region)] <- ""
       
       # Remove empty rows:
       remove <- c("created.by", "dive", "created.at", "updated.by", "updated.at", "sample", "created.by.id", 
@@ -181,9 +217,6 @@ read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = T
                           grep("bad +viz", sections$comment),
                           grep("no +viz", sections$comment))))
       sections$visibility[ix] <- "Poor"
-      
-      # Remove empty columns:
-      if (compress) sections <- gulf.utils::compress(sections)
    }
    
    # Read biological data:
@@ -194,7 +227,7 @@ read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = T
          observations <- rbind(observations, read.csv(paste0(path, "observations/?year=", year[i])))
       } 
    }else{
-      observations <- read.csv(path, "observations/?year")
+      observations <- read.csv(paste0(path, "observations/?year"))
    }
    if (nrow(observations) > 0){
       # Format variable names:
@@ -217,25 +250,39 @@ read.scuba <- function(year, table, compress = TRUE, source = "dmapps", echo = T
       observations$egg.status[is.na(observations$egg.status)] <- ""
       observations$side.display[is.na(observations$side.display)] <- ""
       
-      # Import index variables from sections table: 
+      # Import variables from sections table: 
       ix <- match(observations$section.id, sections$section.id)
+      observations$is.upm        <- as.logical(sections$is.upm[ix])
+      observations$is.training   <- as.logical(sections$is.training[ix])
       observations$dive.id       <- sections$dive.id[ix]
       observations$transect.name <- sections$transect.name[ix]
       observations$diver         <- sections$diver[ix]
+
+      # Format region variable:
+      observations$region <- gsub(" [(]PE[)]", "", observations$region)
+      observations$region[is.na(observations$region)] <- ""
       
       # Re-order variables:
       start <- c("region", "date", "observation.id", "outing.id", "transect.name", "dive.id", "diver", "section.id", "section", "side.display", "species")
       end <- c("comment")
       observations <- observations[, c(start, setdiff(names(observations), c(start, end)), end)]
-      
-      # Remove empty columns:
-      if (compress) observations <- gulf.utils::compress(observations)
    }
    
+   if (compress){
+      # Remove empty columns:
+      outings      <- gulf.utils::compress(outings)
+      dives        <- gulf.utils::compress(dives)
+      sections     <- gulf.utils::compress(sections)
+      observations <- gulf.utils::compress(observations)
+      
+      # Remove empty rows:
+      outings <- outings[which(outings$transect.name != ""), ]
+   } 
+
    # Collate data tables:   
-   res <- list(outings = outings, 
+   res <- list(transects = transects,
+               outings = outings, 
                dives = dives, 
-               transects = transects,
                sections = sections, 
                observations = observations)
    
